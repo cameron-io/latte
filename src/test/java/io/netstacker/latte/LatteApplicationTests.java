a3f451fac6067042329b6e8be72df849109d2896
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-25 21:39:28 +0000

    init

---
+++ src/test/java/io/netstacker/latte/LatteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.netstacker.latte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
