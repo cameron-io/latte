81a5807df9d99a7cacc6835048ad577c837d6919
Cameron Gallichan <camerongallichan@gmail.com>
2024-04-13 23:06:50 +0100

    add Serial annotation to resource exceptions

--- src/main/java/io/netstacker/latte/exception/ResourceAlreadyExistsException.java
+++ src/main/java/io/netstacker/latte/exception/ResourceAlreadyExistsException.java
@@ -3,9 +3,11 @@ package io.netstacker.latte.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(value = HttpStatus.CONFLICT)
 public class ResourceAlreadyExistsException extends Exception{
-
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceAlreadyExistsException(String message){
