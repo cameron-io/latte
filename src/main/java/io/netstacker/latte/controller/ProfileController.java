69a0132203b0ba9c9df21422fd08473068e39232
Cameron Gallichan <camerongallichan@gmail.com>
2024-07-24 13:31:22 +0100

    add Profile controller WIP

--- src/main/java/io/netstacker/latte/controller/ProfileController.java
+++ src/main/java/io/netstacker/latte/controller/ProfileController.java
@@ -1,15 +1,25 @@
 package io.netstacker.latte.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.netstacker.latte.service.ProfileService;
+import io.netstacker.latte.service.UserService;
 
+@RestController
+@RequestMapping("/api/blogs")
 public class ProfileController {
     private final ProfileService profileService;
+    private final UserService userService;
 
     @Autowired
-    public ProfileController(ProfileService profileService) {
+    public ProfileController(
+        ProfileService profileService,
+        UserService userService
+    ) {
         this.profileService = profileService;
+        this.userService = userService;
     }
     
 }
