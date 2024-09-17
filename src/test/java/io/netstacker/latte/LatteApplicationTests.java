016336627084f86d6649222a5471effd2fc2377f
Cameron Gallichan <camerongallichan@gmail.com>
2024-09-17 19:32:15 +0100

    add Http local testing

--- src/test/java/io/netstacker/latte/LatteApplicationTests.java
+++ src/test/java/io/netstacker/latte/LatteApplicationTests.java
@@ -1,13 +1,20 @@
 package io.netstacker.latte;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.netstacker.latte.api.controller.AccountController;
 
 @SpringBootTest
 class LatteApplicationTests {
+    @Autowired
+	private AccountController accountController;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertNotEquals(null, accountController);
 	}
-
 }
