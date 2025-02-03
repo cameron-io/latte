2fb13b0ddb27d385795f643d72ccc11758e35f34
Cameron Gallichan <camerongallichan@gmail.com>
2025-02-03 15:19:13 +0000

    formatting

--- src/main/java/io/netstacker/latte/infra/services/MailService.java
+++ src/main/java/io/netstacker/latte/infra/services/MailService.java
@@ -49,20 +49,14 @@ public class MailService {
             msg.addHeader("Content-Transfer-Encoding", "8bit");
 
             msg.setFrom(new InternetAddress("no_reply@latte.com", "NoReply-JD"));
-
             msg.setReplyTo(InternetAddress.parse("no_reply@latte.com", false));
-
             msg.setSubject(subject, "UTF-8");
-
             msg.setText(body, "UTF-8");
-
             msg.setSentDate(new Date());
-
             msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
             System.out.println("Message is ready");
             Transport.send(msg);
-
-            System.out.println("Email Sent Successfully!!");
         } catch (Exception e) {
             e.printStackTrace();
         }
